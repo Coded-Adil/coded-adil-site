3606a518fe79e3f8fc4cbfdd83522a42c0de88fe
Adil Rahman <itsadil2022@gmail.com>
2025-03-10 23:13:16 +0600

    added some animation to banner of my portfolio

--- src/Components/Banner/Banner.jsx
+++ src/Components/Banner/Banner.jsx
@@ -6,20 +6,26 @@ const Banner = () => {
     return (
         <div className="hero max-w-6xl mx-auto min-h-screen">
             <div className="hero-content gap-6 flex-col lg:flex-row-reverse">
-                <motion.img
-                src={banner}
-                animate={{y: [10, 50, 10]}}
-                transition={{duration: 10, repeat: Infinity}}
-                className="max-w-xs rounded-lg" />
+                <div className='relative'>
+                    <div className='absolute top-0 -left-4 mix-blend-multiply w-48 h-48 md:w-70 md:h-70 bg-purple-300 rounded-full filter blur-xl opacity-70 animate-blob'></div>
+                    <div className='absolute top-0 -right-2 mix-blend-multiply w-48 h-48 md:w-70 md:h-70 bg-sky-300 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-2000'></div>
+                    <div className='absolute -bottom-8 left-20 mix-blend-multiply w-48 h-48 md:w-70 md:h-70 bg-pink-300 rounded-full filter blur-xl opacity-70 animate-blob animation-delay-4000'></div>
+                    <motion.img
+                    src={banner}
+                    animate={{y: [10, 50, 10]}}
+                    transition={{duration: 10, repeat: Infinity}}
+                    className="max-w-xs rounded-lg" />
+                </div>
                 <div className='max-w-xl m-4 animate__animated animate__fadeInLeft'>
                     <h1 className="text-5xl font-code font-bold">Frontend Developer</h1>
                     <p className="py-6 font-exo">
-                    Welcome to my creative corner! I'm Adil Rahman, a passionate frontend developer dedicated 
-                    to building visually stunning, user-friendly, and responsive web experiences. With a keen 
-                    eye for design and a love for clean, efficient code, I transform ideas into interactive 
-                    realities that captivate and engage.
+                        Welcome to my creative corner! I'm <span className='font-bold'>Adil Rahman</span>, a passionate frontend developer dedicated 
+                        to building visually stunning, user-friendly, and responsive web experiences. With a keen 
+                        eye for design and a love for clean, efficient code, I transform ideas into interactive 
+                        realities that captivate and engage.
                     </p>
-                    <p className='py-2 font-exo'>Explore my projects, discover my skills, and let's collaborate to bring your next big 
+                    <p className='py-1 font-exo'>
+                        Explore my projects, discover my skills, and let's collaborate to bring your next big 
                         idea to life!
                     </p>
                     <button className="my-6 font-exo btn btn-info">Contact</button>
