145593f4755ffb195e34b965516d61255c20af17
Adil Rahman <itsadil2022@gmail.com>
2025-03-12 20:54:46 +0600

    added animation to About section in my portfolio

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -1,16 +1,77 @@
+import { GiPenguin } from 'react-icons/gi';
 import adil from '../../../public/adil.jpg'
+import { motion, useMotionValue, useSpring, useTransform } from 'motion/react';
 const About = () => {
+    const x = useMotionValue(0);
+    const y = useMotionValue(0);
+
+    const mouseXSpring = useSpring(x);
+    const mouseYSpring = useSpring(y);
+
+    const rotateX = useTransform(
+        mouseYSpring,
+        [-0.5, 0.5],
+        ["17.5deg", "-17.5deg"]
+    );
+    const rotateY = useTransform(
+        mouseXSpring,
+        [-0.5, 0.5],
+        ["-17.5deg", "17.5deg"]
+    );
+
+    const handleMouseMove = (e) => {
+        const rect = e.target.getBoundingClientRect();
+        const width = rect.width;
+        const height = rect.height;
+        const mouseX = e.clientX - rect.left;
+        const mouseY = e.clientY - rect.top;
+
+        const xPct = mouseX / width - 0.5;
+        const yPct = mouseY / height - 0.5;
+
+        x.set(xPct);
+        y.set(yPct);
+    }
+
+    const handleMouseLeave = () => {
+        x.set(0)
+        y.set(0)
+    }
+
     return (
         <div className="hero max-w-6xl mx-auto min-h-screen">
-            <div className="hero-content flex-col lg:flex-row">
-                <img
-                src={adil}
-                className="max-w-sm rounded-lg shadow-2xl" />
-                <div>
-                    <h1 className="text-5xl font-bold">Hello There!</h1>
+            <div className="hero-content gap-6 flex-col lg:flex-row">
+                <motion.div 
+                className='relative h-[340px] w-[340px] grid place-content-center bg-sky-300 rounded-xl' 
+                onMouseMove={handleMouseMove}
+                onMouseLeave={handleMouseLeave}
+                style={{
+                    rotateX,
+                    rotateY,
+                    transformStyle: 'preserve-3d'
+                }}>
+                    <img
+                    src={adil}
+                    style={{
+                        transform: "translateZ(75px)",
+                    }}
+                    className="max-w-xs rounded-lg shadow-2xl" />
+                </motion.div>
+                <div className='max-w-xl m-4 animate__animated animate__fadeInRight'>
+                    <h1 className=" flex gap-2 text-4xl font-bold"><GiPenguin />About Me</h1>
                     <p className="py-6">
-                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                        Hello! I’m Adil Rahman, a passionate MERN Stack Developer with 8-9 months of hands-on experience 
+                        in building modern, scalable web applications. Currently pursuing my BSc in Computer Science and Engineering, 
+                        I’ve built a strong foundation in programming with C, C++, and Java (OOP), which honed my problem-solving skills 
+                        and sparked my love for coding.
+                        <br />
+                        <br />
+                        I specialize in creating efficient, well-structured, and user-friendly applications. I enjoy every 
+                        step of the development process. I’m quick to learn new technologies and thrive on organizing projects to ensure 
+                        they are scalable and maintainable.
+                        <br />
+                        <br />
+                        Let’s connect and create something amazing together!
                     </p>
                 </div>
             </div>
